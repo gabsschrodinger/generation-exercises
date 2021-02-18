9a1b487d7270e487cf02509cc322d0c08a9beb6a
Gabriel Fernandes Ribeiro <contato.gfernandesribeiro@outlook.com>
2021-02-18 10:02:45 -0300

    Updating spring boot exercise (FEB18)

--- FEB17 - Spring Boot/blog/blog/src/main/java/org/generation/blog/controller/PostController.java
+++ FEB17 - Spring Boot/blog/blog/src/main/java/org/generation/blog/controller/PostController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.generation.blog.model.Post;
 import org.generation.blog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +41,32 @@ public class PostController {
 	public ResponseEntity<List<Post>> GetByTitle(@PathVariable String title) {
 		return ResponseEntity.ok(repository.findAllByTitleContainingIgnoreCase(title));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Post> post (@RequestBody Post post) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(post));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Post> put (@RequestBody Post post) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(post));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
